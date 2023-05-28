e77abdafc2209c581f0c408d43838a5dd439365e
adnanmushtaq1996 <adnanmushtaq5@gmail.com>
2023-05-28 21:46:39 +0200

    Initial Commit

---
+++ src/lambda_function.py
@@ -0,0 +1,21 @@
+__author__      = "Adnan Karol"
+__version__ = "1.0.1"
+__maintainer__ = "Adnan Karol"
+__email__ = "adnanmushtaq5@gmail.com"
+__status__ = "Dev"
+
+
+# Import Dependencies
+import pandas as pd
+import numpy as np
+
+
+# Helper (Main) Function for AWS Lambda
+def handler(event, context):
+    
+
+
+    return {
+		"statusCode": 200,
+		"message": "Invocation Successfull"
+	}
